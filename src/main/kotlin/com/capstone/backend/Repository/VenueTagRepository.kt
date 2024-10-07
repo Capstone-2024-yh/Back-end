a80362f3568719973cb11b3355037da47e442a6e
newdongjun <shindj4613@outlook.com>
2024-10-07 16:43:25 +0900

    유사도 검색 쿼리 구현 전 임시

--- src/main/kotlin/com/capstone/backend/Repository/VenueTagRepository.kt
+++ src/main/kotlin/com/capstone/backend/Repository/VenueTagRepository.kt
@@ -2,6 +2,8 @@ package com.capstone.backend.Repository
 
 import com.capstone.backend.Entity.VenueTag
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -12,4 +14,9 @@ interface VenueTagRepository : JpaRepository<VenueTag, Long> {
 
     // tag로 검색하는 메서드
     fun findByTag(tag: String): List<VenueTag>
+
+//    벡터로 유사 검색하는 쿼리 나중에 구현 해야 함 지금 그냥 임시 쿼리임
+    @Query("SELECT v FROM VenueTag v")  // 모든 VenueTag 반환 (임시 쿼리)
+    fun findBySimilarTag(@Param("vector") vector: List<Double>): List<VenueTag>
+
 }
