f38d82e70e26f204f813d6099d85ce693844fbc0
newdongjun <shindj4613@outlook.com>
2024-11-19 23:46:23 +0900

    api path edit

--- src/main/kotlin/com/capstone/backend/controllers/SearchTokenController.kt
+++ src/main/kotlin/com/capstone/backend/controllers/SearchTokenController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/search-tokens")
+@RequestMapping("/search-token")
 class SearchTokenController(
     private val searchTokenService: SearchTokenService
 ) {
